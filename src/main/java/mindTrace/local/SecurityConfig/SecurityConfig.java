c42c6250f3508b77c6d6ea2bc296fdb664adbf95
Yassine Azami Idrissi <=>
2025-10-31 17:36:02 +0100

    Cors config

--- src/main/java/mindTrace/local/SecurityConfig/SecurityConfig.java
+++ src/main/java/mindTrace/local/SecurityConfig/SecurityConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
@@ -23,5 +29,23 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(
+                Collections.singletonList("http://localhost:3000")
+        );
+        config.setAllowedHeaders(
+                Arrays.asList("*"));
+        config.setAllowedMethods(
+                Arrays.asList("*")
+        );
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
